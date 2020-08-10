ea8bc857ddae04193bc133d100305044bb660517
sala <1552923147@qq.com>
2020-08-10 18:06:53 +0800

    1

--- src/miniprogram/components/voice/index.js
+++ src/miniprogram/components/voice/index.js
@@ -1,7 +1,7 @@
 
 let plugin = requirePlugin("QCloudAIVoice");
 let manager = plugin.getRecordRecognitionManager()
-plugin.setQCloudSecret(1302473051, 'AKIDdz5VfYsBcpthJfs65sERO5VekZ7QMXIn', 'TO4XmsnWy9G6g1LOUJq9D0WoIVR0OVx8', true); 
+plugin.setQCloudSecret(1302473051, 'AKIDvafTyD2uf9O5Wdie4C2gYDYhbFdN799s', 'e2A2eHdttbMrFNE8lIYquze3BNek59xO', true); 
 Component({
   properties: {
     authority:Boolean
